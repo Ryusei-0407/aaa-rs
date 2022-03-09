ecb631186ad79431f317432d03ee1ec4490384c2
ryu <ryu000407@gmail.com>
2022-03-10 08:15:46 +0900

    create ecc

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+#[derive(Debug, PartialEq)]
+struct FieldElement {
+    num: isize,
+    prime: isize,
+}
+
+impl FieldElement {
+    fn new(num: isize, prime: isize) -> FieldElement {
+        if num >= prime || num < 0 {
+            println!("Num {} not in field range 0 to {}", num, prime - 1)
+        };
+
+        FieldElement { num, prime }
+    }
+}
+
+#[test]
+fn ecc() {
+    let a = FieldElement::new(7, 13);
+    let b = FieldElement::new(6, 13);
+
+    assert_eq!(a, a);
+    assert_ne!(a, b);
+}
+
+fn main() {}
